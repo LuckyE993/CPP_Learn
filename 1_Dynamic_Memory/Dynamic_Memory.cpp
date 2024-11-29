ed403c94fda4c349a48e7f02f3856e03b0904418
Sun Qixuan <90081491+LuckyE993@users.noreply.github.com>
2024-11-29 11:26:27 +0800

    Dynamic_Memory.cpp

--- 1_Dynamic_Memory/Dynamic_Memory.cpp
+++ 1_Dynamic_Memory/Dynamic_Memory.cpp
@@ -3,13 +3,29 @@
 //
 #include <iostream>
 #include <memory>
+#include <cstring>
 using namespace std;
+
+class Box
+{
+public:
+    Box()
+    {
+        cout << "Constructor called!" << endl;
+    }
+
+    ~Box()
+    {
+        cout << "Destructor called!" << endl;
+    }
+};
+
 
 int main()
 {
-    double* pvalue = NULL; // Pointer initialized with null
+    double* pvalue = nullptr; // Pointer initialized with null
 
-    if(!((pvalue = new double)))
+    if (!((pvalue = new double)))
     {
         cout << "Error: out of memory." << endl;
         exit(1);
@@ -19,6 +35,35 @@ int main()
     cout << "Value of pvalue: " << *pvalue << endl;
 
     delete pvalue;
+
+    char* pcharvalue = nullptr; // Pointer initialized with null
+
+    pcharvalue = new char [20];
+    strcpy(pcharvalue, "LuckyE");
+    cout << "Value of pcharvalue: " << pcharvalue << endl;
+
+    delete [] pcharvalue;
+
+    cout << "--------------------------------" << endl;
+
+    int** pMulti = nullptr;
+    pMulti = new int*[3]; // 分配 3 个指针
+    // 为每个指针分配大小为 4 的数组
+    for (int i = 0; i < 3; ++i)
+    {
+        pMulti[i] = new int[4];
+    }
+    // 使用完毕后记得释放内存
+    for (int i = 0; i < 3; ++i)
+    {
+        delete[] pMulti[i]; // 释放每一行
+    }
+    delete[] pMulti; // 释放主指针数组
+
+    cout << "--------------------------------" << endl;
+
+    Box* pbox = new Box[4];
+    delete[] pbox;
 
     return 0;
 }
